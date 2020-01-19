410b7318055fc90b54533fb374bb08a570eec134
Juan Carlos Ballesteros Hermida <jcballesteroshermida@gmail.com>
2020-01-19 16:53:19 +0100

    Actualización usando listas.

---
+++ voraz/README.md
@@ -0,0 +1,12 @@
+# Ejercicio Algoritmo Voraz
+En Tecnificados hemos organizado un festival de cine de terror. Durante 24 horas se proyectarán N películas diferentes en las 10 salas disponibles de cine de Metro City (cada película sólo se proyectará una vez a lo largo del festival). En la programación aparecen todas las películas que se van a proyectar, junto con el título, duración de la película y otros datos, se indica la sala de proyección y la hora de comienzo.
+
+
+
+Una persona desea planificar su maratón de cine, teniendo en cuenta que el único objetivo es ver el máximo número posible de películas. 
+
+
+
+Debemos realizar una algoritmos voraz para realizar ese maratón.
+
+Más info en tecnificados.com
--- voraz/src/main/java/org/tecnificados/algoritmos/voraz/SolucionVoraz.java
+++ voraz/src/main/java/org/tecnificados/algoritmos/voraz/SolucionVoraz.java
@@ -1,140 +1,158 @@
 package org.tecnificados.algoritmos.voraz;
 
-public class SolucionVoraz
-{
-  //Vector de peliculas
-  Pelicula laLista[];
-  //Vector con las peliculas elegidas
-  Pelicula laSolucion[];
-  //Numero de peliculas en cartel
-  int tam = 0;
+import java.util.ArrayList;
+import java.util.List;
 
-  public SolucionVoraz()
-  {
-    laLista = new Pelicula[20];
-    laSolucion = new Pelicula[20];
-  }
-  //funcion que devuelve el tama�o
-  public int dameTam() {return tam;}
-  //funcion que devuelve una pelicula de la lista
-  public Pelicula damePelicula(int p) {return laLista[p];}
-  //funcion que a�ade una pelicula
-  public void add(Pelicula P)
-  {
-    laLista[tam++] = P;
-  }
-  //funcion toString
-  public String toString()
-  {
-    String Cadena = "";
-    for (int i = 0; i < tam; i++)
-      Cadena = Cadena + laLista[i] + "\n";
-    return Cadena;
-  }
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  //funcion cambia que se usa para el quicksort
-  private void cambia(Pelicula vector[], int j, int k)
-  {
-    Pelicula aux = vector[j];
-    vector[j] = vector[k];
-    vector[k] = aux;
-  }
-  //funcion particion que se usa para el quicksort
-  private int particion(Pelicula a[], int m, int n) {
-    Pelicula x = a[m]; // Pivote
-    int j = n + 1;
-    int i = m - 1;
-    while (true) {
-      j--;
-      while ( (a[j]).dameTiempo() > x.dameTiempo())
-        j--;
-      i++;
-      while ( (a[i]).dameTiempo() < x.dameTiempo())
-        i++;
-      if (i < j)
-        cambia(a, i, j);
-      else
-        return j;
-    }
-  }
-  //funcion que ordena el vector de las peliculas por hora y duracion
-  public void quickSort(int l, int r) {
-    //La funcion en si
-    if (l < r) {
-      int aux = particion(laLista, l, r);
-      quickSort(l, aux);
-      quickSort(aux + 1, r);
-    }
-  }
+/**
+ * @author Juan Carlos Ballesteros (tecnificados.com)
+ */
+public class SolucionVoraz {
 
-  //funcion que elige las peliculas usando algoritmo voraz
-  public void solucion() {
-    //Empiezo a escoger
-    int pasadas = 0;
-    int solucion = 0;
-    double Horafin = 0;
-    while (pasadas < dameTam()) {
-      //cogo una pelicula
-      Pelicula aux = damePelicula(pasadas);
-      //miro a que hora acaba
-      Horafin = aux.dameTiempo();
-      //la imprimo por pantalla
-      System.out.println(aux);
-      //la guardo en el vector
-      laSolucion[solucion++]=aux;
-      //miro la siguiente
-      pasadas++;
-      //si me cabe la eligo, en caso contrario busco la siguiente
-      if (pasadas < dameTam()) {
-        while (damePelicula(pasadas).dameHora() < Horafin)
-          pasadas++;
-      }
-    }
-  }
+	private static final Logger log = LoggerFactory.getLogger(SolucionVoraz.class);
 
-  public static void main(String[] args)
-  {
-    //creo las peliculas  (considero que las medias horas son de 50, 9.30=9.50)
-    Pelicula p1 = new Pelicula("El exorcista", 90, 21.50, 1);
-    Pelicula p2 = new Pelicula("Pesadilla en Elm Street 4", 120, 9.50, 2);
-    Pelicula p3 = new Pelicula("La Pesadilla final", 96, 10.50, 3);
-    Pelicula p4 = new Pelicula("Reanimator", 90, 12.50, 4);
-    Pelicula p5 = new Pelicula("La Nueva Pesadilla", 120, 12.50, 5);
-    Pelicula p6 = new Pelicula("Viernes 13", 100, 14.00, 6);
-    Pelicula p7 = new Pelicula("Apocalipsis", 223, 15.00, 7);
-    Pelicula p8 = new Pelicula("It", 183, 16.50, 8);
-    Pelicula p9 = new Pelicula("Halloween", 74, 17.30, 9);
-    Pelicula p10 = new Pelicula("El Ente", 96, 17.00, 10);
-    Pelicula p11 = new Pelicula("Jason X", 81, 17.00, 11);
-    Pelicula p12 = new Pelicula("La Celda", 123, 15.00, 12);
-    Pelicula p13 = new Pelicula("Horizonte Final", 87, 14.00, 13);
-    Pelicula p14 = new Pelicula("Destino Final", 95, 16.00, 14);
-    Pelicula p15 = new Pelicula("Mortal Zombie", 93, 17.50, 15);
-    //Creo la clase ejercicio_9
-    SolucionVoraz Peliculas = new SolucionVoraz();
-    //A�ado las peliculas
-    Peliculas.add(p1);
-    Peliculas.add(p2);
-    Peliculas.add(p3);
-    Peliculas.add(p4);
-    Peliculas.add(p5);
-    Peliculas.add(p6);
-    Peliculas.add(p7);
-    Peliculas.add(p8);
-    Peliculas.add(p9);
-    Peliculas.add(p10);
-    Peliculas.add(p11);
-    Peliculas.add(p12);
-    Peliculas.add(p13);
-    Peliculas.add(p14);
-    Peliculas.add(p15);
-    //imprimo las peliculas
-    System.out.println(Peliculas);
-    //ordeno las peliculas por hora y duracion
-    Peliculas.quickSort(0, 14);
-    //imprimo las peliculas ordenadas
-    System.out.println(Peliculas);
-    //llamo a la funcion solucion
-    Peliculas.solucion();
-    }
+	// Peliculas disponibles
+	List<Pelicula> peliculasEnCartel=new ArrayList<Pelicula>();
+	// Solución
+	List<Pelicula> laSolucion=new ArrayList<Pelicula>();
+
+
+	public SolucionVoraz() {
+		peliculasEnCartel=new ArrayList<Pelicula>();
+		laSolucion=new ArrayList<Pelicula>();
+	}
+
+	
+
+
+
+	// funcion que a�ade una pelicula
+	public void add(Pelicula peli) {
+		peliculasEnCartel.add(peli);
+	}
+
+	// funcion toString
+	public String toString() {
+		String Cadena = "";
+		for (Pelicula peli:peliculasEnCartel)
+			Cadena = Cadena + peli.toString() + "\n";
+		return Cadena;
+	}
+
+	// funcion cambia que se usa para el quicksort
+	private void cambia(List<Pelicula> lista, int j, int k) {
+		Pelicula aux = lista.get(j);
+		lista.set(j,lista.get(k));
+		lista.set(k, aux);
+	}
+
+	// funcion particion que se usa para el quicksort
+	private int particion(List<Pelicula> a, int m, int n) {
+		Pelicula x = a.get(m); // Pivote
+		int j = n + 1;
+		int i = m - 1;
+		while (true) {
+			j--;
+			while ((a.get(j)).dameTiempo() > x.dameTiempo())
+				j--;
+			i++;
+			while ((a.get(i)).dameTiempo() < x.dameTiempo())
+				i++;
+			if (i < j)
+				cambia(a, i, j);
+			else
+				return j;
+		}
+	}
+
+	// funcion que ordena el vector de las peliculas por hora y duracion
+	public void quickSort(int l, int r) {
+		if (l < r) {
+			int aux = particion(peliculasEnCartel, l, r);
+			quickSort(l, aux);
+			quickSort(aux + 1, r);
+		}
+	}
+
+	// funcion que elige las peliculas usando algoritmo voraz
+	public void solucion() {
+		// Empiezo a escoger
+		int pasadas = 0;
+		double Horafin = 0;
+		while (pasadas < peliculasEnCartel.size()) {
+			// cogo una pelicula
+			Pelicula aux = peliculasEnCartel.get(pasadas);
+			// miro a que hora acaba
+			Horafin = aux.dameTiempo();
+			// la imprimo por pantalla
+			log.info(aux.toString());
+			// la guardo en el vector
+			laSolucion.add(aux);
+			// miro la siguiente
+			pasadas++;
+			// si me cabe la eligo, en caso contrario busco la siguiente
+			if (pasadas < peliculasEnCartel.size()) {
+				while (peliculasEnCartel.get(pasadas).dameHora() < Horafin)
+					pasadas++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+	
+		// creo las peliculas (considero que las medias horas son de 50, 9.30=9.50)
+		Pelicula p1 = new Pelicula("El exorcista", 90, 21.50, 1);
+		Pelicula p2 = new Pelicula("Pesadilla en Elm Street 4", 120, 9.50, 2);
+		Pelicula p3 = new Pelicula("La Pesadilla final", 96, 10.50, 3);
+		Pelicula p4 = new Pelicula("Reanimator", 90, 12.50, 4);
+		Pelicula p5 = new Pelicula("La Nueva Pesadilla", 120, 12.50, 5);
+		Pelicula p6 = new Pelicula("Viernes 13", 100, 14.00, 6);
+		Pelicula p7 = new Pelicula("Apocalipsis", 223, 15.00, 7);
+		Pelicula p8 = new Pelicula("It", 183, 16.50, 8);
+		Pelicula p9 = new Pelicula("Halloween", 74, 17.30, 9);
+		Pelicula p10 = new Pelicula("El Ente", 96, 17.00, 10);
+		Pelicula p11 = new Pelicula("Jason X", 81, 18.00, 1);
+		Pelicula p12 = new Pelicula("La Celda", 123, 15.00, 2);
+		Pelicula p13 = new Pelicula("Horizonte Final", 87, 16.00, 3);
+		Pelicula p14 = new Pelicula("Destino Final", 95, 17.00, 4);
+		Pelicula p15 = new Pelicula("Mortal Zombie", 93, 17.50, 5);
+
+		SolucionVoraz Peliculas = new SolucionVoraz();
+
+		Peliculas.add(p1);
+		Peliculas.add(p2);
+		Peliculas.add(p3);
+		Peliculas.add(p4);
+		Peliculas.add(p5);
+		Peliculas.add(p6);
+		Peliculas.add(p7);
+		Peliculas.add(p8);
+		Peliculas.add(p9);
+		Peliculas.add(p10);
+		Peliculas.add(p11);
+		Peliculas.add(p12);
+		Peliculas.add(p13);
+		Peliculas.add(p14);
+		Peliculas.add(p15);
+		// imprimo las peliculas
+		log.info("****************Cartel****************");
+		for (Pelicula p:Peliculas.peliculasEnCartel)
+		{
+			log.info(p.toString());
+		}
+		// ordeno las peliculas por hora y duracion
+		Peliculas.quickSort(0, 14);
+		// imprimo las peliculas ordenadas
+		log.info("****************Ordenadas****************");
+		for (Pelicula p:Peliculas.peliculasEnCartel)
+		{
+			log.info(p.toString());
+		}
+		// llamo a la funcion solucion
+		log.info("****************Solución****************");
+		Peliculas.solucion();
+	}
 }
